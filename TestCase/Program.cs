0779edb7a9cf711bd3a15e7e93d82610c5f1d199
AzozoCode <josephbarrigah@gmail.com>
2023-07-12 09:26:04 +0000

    add:seedData funcction

--- TestCase/Program.cs
+++ TestCase/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HrManagementLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,64 @@ namespace TestCase
         {
 
             
-            
-              
-
             Console.ReadLine();
         }
 
+        public static void SeedData(List<IEmployee> employee)
+        {
+            IEmployee teacher1 = new Teacher
+            {
+                Id = 1,
+                FirstName = "David",
+                LastName = "Moore",
+                Salary = 4000
+
+            };
+
+            employee.Add(teacher1);
+
+            IEmployee teacher2 = new Teacher
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Banner",
+                Salary = 4500
+            };
+
+            employee.Add(teacher2);
+
+            IEmployee headOfDepartment = new HeadOfDepartment { 
+            
+             Id = 3,
+             FirstName = "Mario",
+             LastName = "Kings",
+             Salary = 6000
+            };
+
+            employee.Add(headOfDepartment);
+
+            IEmployee deputyHeadMaster = new DeputyHeadMaster
+            {
+                Id = 4,
+                FirstName = "Michael",
+                LastName = "Nartey",
+                Salary = 7500
+            };
+
+            employee.Add(deputyHeadMaster);
+
+            IEmployee headMaster = new HeadMaster {
+                Id = 5,
+                FirstName = "Joseph",
+                LastName = "Hagan",
+                Salary = 8000
+            
+            };
+
+            employee.Add(headMaster);
+        }
+
+        
 
         
     }
